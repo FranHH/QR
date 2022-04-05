5b53f92e4816d563d9f2db57483fc94f2c7e5235
FranDev <fjperez@iccaweb.com>
2022-04-05 12:53:03 +0200

    CreateProject

---
+++ QR/QR/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace QR
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            NavigationPage.SetHasNavigationBar(this, false);
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage())
+            {
+                BarBackgroundColor = Color.DarkGray
+            };
+
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ QR/QR/Views/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using QR.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using ZXing.Net.Mobile.Forms;
+
+namespace QR
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnScan_Clicked(object sender, EventArgs e)
+        {
+            var scan = new ZXingScannerPage();
+            await QR.App.Current.MainPage.Navigation.PushModalAsync(scan);
+            scan.OnScanResult += (result) =>
+            {
+                Device.BeginInvokeOnMainThread(async () =>
+                {
+                    await QR.App.Current.MainPage.Navigation.PopModalAsync();
+                    if (!string.IsNullOrEmpty(result.Text))
+                        txtBarCode.Text = result.Text;
+                });
+            };
+        }
+
+
+        private async void btnHistory(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new History() 
+            {
+                BackgroundColor = Color.DarkGray
+
+            }, false);
+        }
+
+    }
+}
